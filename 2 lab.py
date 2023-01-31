31bea344a9360da918c2bedb47327c20e3d85c87
AitykMusa <123458100+AitykMusa@users.noreply.github.com>
2023-01-31 16:47:28 +0600

    Add files via upload

---
+++ 2 lab.py
@@ -0,0 +1,12 @@
+Name_user = input ('Ваша имя: ')
+Surname = input ('Ваша фамилия: ')
+print(f'Ваше Имя {Name_user') Ваша фамилия {surname}')
+
+old = int(input('Ваш возраст: '))
+
+if 18 > old:
+    print("Вы совершолетний")
+else:
+    18 < old
+    print("Вы совершеннолетний")
+    
